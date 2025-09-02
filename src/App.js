722c85d0411931fec7dfc8daf89170026617738b
Karthik Kovi <karthik.kovi2001@gmail.com>
2025-09-02 12:43:13 -0700

    My Portfolio New UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import Header from './Components/Header';
-import About from './Components/About';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
-import HomeScreen from './Components/homescreen';
-
+import './App.css';
+import Portfolio from './Portfolio';
 
 function App() {
   return (
-    <>
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <HomeScreen />
-      <div className="container mx-auto px-4">
-        <About />
-        <Projects />
-        <Contact />
-      </div>
+    <div className="App">
+      <Portfolio />
     </div>
-    </>
   );
 }
 
